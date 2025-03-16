5cfbbb842dbee3cdfff083573a0dae57e7cb27e2
Abdul Rehman <abdulrehmanmangral86@gmail.com>
2025-03-16 16:29:33 +0500

    fix bugs

--- celery_beat_config.py
+++
@@ -1,9 +0,0 @@
-from celery.schedules import crontab
-from celery_config import celery
-
-celery.conf.beat_schedule = {
-    "check-prices-every-5-minutes": {
-        "task": "app.crud.user_alert.run_price_check",
-        "schedule": crontab(minute="*/5"),  # Every 5 minutes
-    },
-}
--- celery_config.py
+++ celery_config.py
@@ -1,9 +1,9 @@
 from celery import Celery
-
+from celery.schedules import crontab
 
 celery = Celery(
     "worker",
-    broker="redis://redis:6379/0",  # Use service name in Docker Compose as hostname
+    broker="redis://redis:6379/0",
     backend="redis://redis:6379/0",
     include=["app.crud.user_alert"],
 )
@@ -14,6 +14,13 @@ celery.conf.update(
     },
     timezone="UTC",
     enable_utc=True,
+    beat_schedule={
+        "check-prices-every-1-minutes": {
+            "task": "app.crud.user_alert.run_price_check",
+            "schedule": crontab(minute="*/1"),
+        }
+    },
+    result_backend="redis://redis:6379/0",  # Ensures Beat schedule is also tracked in Redis
 )
 
 if __name__ == "__main__":
